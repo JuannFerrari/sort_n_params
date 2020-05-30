b46325aa97fd1f50b539ccf5a103f75113ab8c79
Matias Conde <matias.conde@snappler.com>
2020-05-30 15:49:40 -0300

    added tests for parse_array method

--- lib/sort_n_params/concerns/scopes.rb
+++ lib/sort_n_params/concerns/scopes.rb
@@ -10,7 +10,7 @@ module SortNParams
         all.order(parse_array(ordering_array, table_name))
       end
 
-      def parse_array(ordering_array, table_name)
+      def parse_array(ordering_array, table_name = nil)
         table = table_name || name.tableize
 
         ordering_array.each_slice(2).map do |order, direction|
